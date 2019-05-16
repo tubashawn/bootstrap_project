9959a6c50e15feda48e1f309f3ed8517de34d7d0
tubaShawn <shawnrmangum@gmail.com>
2019-05-15 22:12:06 -0600

    added scrollspy

--- assets/js/interactive.js
+++ assets/js/interactive.js
@@ -2,4 +2,9 @@ $(function() {
     $(window).on("load resize", function() {
         $(".fill-screen").css("height", window.innerHeight);
     }) ;
+
+    // Bootstrap scrollspy
+    $("body").scollspy({
+        target: ".navbar"
+    })
 });
